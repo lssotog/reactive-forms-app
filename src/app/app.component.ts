11751988bdaf2acd9e5a47d9966134983d690c9e
sotol067 <luz.soto.-nd@disney.com>
2024-04-15 17:52:47 -0500

    Added formGroup and FormControl to AppComponent. Used get() to obtain value of the input (this way was used on V12 and lower versions. In this case the value's type is any)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,12 +15,26 @@ import { MatInput } from '@angular/material/input';
     MatFormFieldModule, 
     MatIcon,
     MatInput, 
+    ReactiveFormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'reactive-forms-app'
-  clickRegister(): void {}
+  title = 'reactive-forms-app';
+  @Input() userName: string = "Reactive forms App";
+  formGroup = new FormGroup({
+    names: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+
+  clickRegister(): void {
+    this.userName = `The user name is ${this.formGroup.get('names')?.value as string}`;
+    console.log("name value --->", this.userName);
+  }
 }
  
