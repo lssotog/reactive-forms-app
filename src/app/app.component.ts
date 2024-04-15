d44b07ee216e2964e0743d63dda7be0ca84b6238
sotol067 <luz.soto.-nd@disney.com>
2024-04-15 16:14:17 -0500

    Styles - Angular Material and form added on AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,     
+    MatButton,
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatIcon,
+    MatInput, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'reactive-forms-app';
+  title = 'reactive-forms-app'
+  clickRegister(): void {}
 }
+ 
